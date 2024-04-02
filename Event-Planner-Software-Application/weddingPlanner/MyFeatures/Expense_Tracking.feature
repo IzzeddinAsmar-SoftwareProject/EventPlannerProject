5a37bddcda5ecd44ecfe65e988e7f3d8a909a93a
Izzeddin Asmar <160132501+IzzeddinAsmar@users.noreply.github.com>
2024-04-02 21:00:38 +0200

    Delete Event-Planner-Software-Application directory

--- Event-Planner-Software-Application/weddingPlanner/MyFeatures/Expense_Tracking.feature
+++
@@ -1,6 +0,0 @@
-Feature: Expense Tracking
-  Background:
-    Scenario: User Makes a Reservation
-    Given User role is "USER"
-    When User Makes a Reservation
-    Then User Expense Should Be Tracked Under Hall Reservation Category
--- Event-Planner-Software-Application/weddingPlanner/src/test/java/wedding/Planner/RegularUser.java
+++
@@ -1,8 +0,0 @@
-package wedding.Planner;
-
-public class RegularUser extends User {
-    public RegularUser(String username, String password, String hallNumber) {
-        super(username, password, "USER", hallNumber); // Pass hallNumber to the superclass constructor
-    }
-
-}
